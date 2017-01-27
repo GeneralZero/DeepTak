8777eccef5844260f2592a0f6c20e07484122ab8
Brian Ridings <bdrid001@gmail.com>
2017-01-27 02:07:11 -0500

    Update download script

--- playtak.py
+++ playtak.py
@@ -2,29 +2,32 @@ import sqlite3, os.path, requests
 
 class PlayTak(object):
 	"""Utility to get playtak.com data"""
-	def __init(self):
-		if !os.path.isfile("games_anon.db"):
+	def __init__(self):
+		if not os.path.isfile("games_anon.db"):
 			self.download_sqllite()
 		self.connection = sqlite3.connect("games_anon.db")
-		self.cursor = connection.cursor()
+		self.cursor = self.connection.cursor()
 
 		self.cursor.execute("""SELECT notation FROM games WHERE games.result != "1-0" OR games.result != "0-1" """)
 		self.notation_array = self.cursor.fetchall() 
+		#print(self.notation_array)
 
 	def download_sqllite(self):
-		r = requests.get("https://www.playtak.com/games_anon.db")
-		with open('games_anon.db', 'bw') as the_file:
-			the_file.write(r.content)
+		r = requests.get("https://www.playtak.com/games_anon.db", stream=True)
+		
+		if r.status_code == 200:
+			with open('games_anon.db', 'wb') as f:
+				f.write(r.content)
 
 	def parse_server_to_dict(self):
 		player_moves = []
 		for move in moves.split(","):
-			if move[0].lower = "p":
+			if move[0].lower == "p":
 				# Place a piece
 				# Ex P E3 W
 				placement, piece = re.match("([a-zA-z][0-9]+)\w?([WC])", move[2:])
 				player_moves.append({"movetype": move[0], "placement": placement, "piece": piece})
-			elif move[0].lower = "m":
+			elif move[0].lower == "m":
 				# Move a piece
 				# M FROM TO #PLACE+
 				start, end, countstring = re.match("([a-zA-z][0-9]+)\w([a-zA-z][0-9]+)\w([0-9\w]+)", move[2:])
@@ -44,12 +47,14 @@ class PlayTak(object):
 def valid_moves(board, player):
 	all_moves = []
 	#All of the places
-	for index, open_squares in enumerate(board) if len(open_squares) == 0:
+	#for index, open_squares in enumerate(board) if len(open_squares) == 0:
+	if 1==1:
 		if player.used_capstone():
 			for types in ["F","W"]:
 				all_moves.append({"movetype": move[0], "placement": placement, "piece": piece})
 		else:
-			for types in ["F","W", "C"]:
+			#for types in ["F","W", "C"]:
+			pass
 	#All of the types
 
 	#All of the Stacks
@@ -59,3 +64,6 @@ def valid_moves(board, player):
 
 
 
+if __name__ == '__main__':
+	p = PlayTak()
+	#p.download_sqllite()
